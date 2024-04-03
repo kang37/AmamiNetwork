8000b35251ef66abece1a66454feb4b251541b09
Kang <kangjf1943@gmail.com>
2024-04-03 14:57:38 +0900

    feat: log number and stay time of each dailyid-segment for each cluster

--- cluster.R
+++ cluster.R
@@ -114,3 +114,105 @@ ggplot() +
     data = filter(gis_agoop_smp_coord, cluster != 0),
     aes(col = as.character(cluster)), alpha = 0.5
   )
+
+# Trajectory between clusters ----
+# About 500 dailyid in the data. What are their average stay time?
+# Bug: How to id stay or pass? Method 1: by personal proportion. For example, if one stay in a place much of a day, then s/he stays there; if the proportion is only 1%, then it is pass. But problem: you only need to spend 10 min visiting a visitor center, isn't that "stay"?
+gis_agoop_smp_coord %>%
+  st_drop_geometry() %>%
+  filter(dailyid == temp_id_num$dailyid[2]) %>%
+  group_by(cluster) %>%
+  summarise(prop = n(), .groups = "drop") %>%
+  reframe(prop = prop / sum(prop))
+
+# Method 2: time distribution of all visitors in a cluster.
+# Bug: Aproximate stay time with the number of logs. Density of log numbers of each dailyid in a cluster.
+lapply(
+  0:10,
+  function(cluster_id) {
+    gis_agoop_smp_coord %>%
+      st_drop_geometry() %>%
+      filter(cluster == cluster_id) %>%
+      group_by(dailyid) %>%
+      summarise(n = n(), .groups = "drop") %>%
+      mutate(cluster_id = cluster_id)
+  }
+) %>%
+  bind_rows() %>%
+  ggplot() +
+  geom_density(aes(n)) +
+  facet_wrap(.~ cluster_id, scales = "free")
+# Seems there is no obvious diff between "pass" and "stay."
+
+# Should further divide segments: a dailyid has more than one segment even for a cluster. For instance, the pathway c1-c2-c1-c3 has 2 c1 segments.
+# Should further divid segments: a dailyid has more than one segment even for a cluster. For instance, the pathway c1-c2-c1-c3 has 2 c1 segments.
+# Bug: Should make a time column for the raw data before.
+gis_agoop_smp_coord <- gis_agoop_smp_coord %>%
+  mutate(time = as_datetime(
+    paste(
+      paste(year, month, day, sep = "-"),
+      paste(hour, minute, "00", sep = "-"),
+      sep = " "
+    )
+  )) %>%
+  # Bug: Remove one-dailyid-in-two
+  arrange(dailyid, time, accuracy) %>%
+  group_by(dailyid, time) %>%
+  mutate(time_conflict_id = row_number()) %>%
+  filter(time_conflict_id == 1) %>%
+  select(-time_conflict_id)
+
+# Get segment ID for each dailyid.
+seg_id <- gis_agoop_smp_coord %>%
+  st_drop_geometry() %>%
+  arrange(dailyid, time) %>%
+  group_by(dailyid) %>%
+  mutate(
+    new_dailyid = c(dailyid != lag(dailyid)),
+    new_cluster = c(cluster != lag(cluster)),
+    new_dailyid = case_when(is.na(new_dailyid) ~ TRUE, TRUE ~ new_dailyid),
+    new_cluster = case_when(is.na(new_cluster) ~ TRUE, TRUE ~ new_cluster)
+  ) %>%
+  select(dailyid, time, cluster, new_dailyid, new_cluster) %>%
+  filter(new_dailyid + new_cluster >= 1) %>%
+  mutate(seg_id = row_number())
+
+seg <- gis_agoop_smp_coord %>%
+  st_drop_geometry() %>%
+  arrange(dailyid, hour) %>%
+  left_join(seg_id, by = c("dailyid", "cluster", "time")) %>%
+  ungroup() %>%
+  fill(seg_id)
+
+library(tidyr)
+# Distribution of log numbers of each segment for the clusters.
+lapply(
+  0:10,
+  function(cluster_id) {
+    seg %>%
+      filter(cluster == cluster_id) %>%
+      group_by(dailyid, seg_id) %>%
+      summarise(n = n(), .groups = "drop") %>%
+      mutate(cluster_id = cluster_id)
+  }
+) %>%
+  bind_rows() %>%
+  ggplot() +
+  geom_density(aes(n)) +
+  facet_wrap(.~ cluster_id, scales = "free")
+
+# Distribution of stay time of each segment for the clusters.
+lapply(
+  0:10,
+  function(cluster_id) {
+    seg %>%
+      filter(cluster == cluster_id) %>%
+      group_by(dailyid, seg_id) %>%
+      summarise(stay_time = max(time) - min(time), .groups = "drop") %>%
+      mutate(cluster_id = cluster_id)
+  }
+) %>%
+  bind_rows() %>%
+  ggplot(aes(as.character(cluster_id), stay_time)) +
+  geom_boxplot() +
+  geom_jitter()
